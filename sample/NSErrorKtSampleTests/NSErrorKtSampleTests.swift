9307405ac53a2d486dd34beec58ac26305724db0
Rick Clephas <rclephas@gmail.com>
2022-02-12 17:13:06 +0100

    Update sample

--- sample/NSErrorKtSampleTests/NSErrorKtSampleTests.swift
+++ sample/NSErrorKtSampleTests/NSErrorKtSampleTests.swift
@@ -10,33 +10,47 @@ import NSErrorKtSampleShared
 @testable import NSErrorKtSample
 
 class NSErrorKtSampleTests: XCTestCase {
+
+    func testFromKotlin() {
+        let error = NSErrorTestsKt.testAsNSError() as NSError
+        XCTAssertEqual("KotlinException", error.domain)
+        XCTAssertEqual(0, error.code)
+        XCTAssertTrue(NSErrorKt.isThrowable(error))
+        let exception = error.userInfo["KotlinException"] as! KotlinThrowable
+        XCTAssertFalse(exception.isNSError)
+        let throwable = NSErrorKt.asThrowable(error)
+        XCTAssertEqual(exception, throwable)
+    }
     
-    class Test: NSErrorTest {
-        
-        let nsError: NSError
-        
-        init(nsError: NSError) {
-            self.nsError = nsError
-        }
-        
-        func test(callback: NSErrorCallback) {
-            callback.onCompleted(error: nsError)
-        }
+    func testFromSwift() {
+        let error = NSError(domain: "Test Domain", code: 1, userInfo: nil)
+        XCTAssertFalse(NSErrorKt.isThrowable(error))
+        let throwable = NSErrorTestsKt.testAsThrowable(nsError: error)
+        XCTAssertTrue(throwable.isNSError)
+        let nsError = throwable.asNSError() as NSError
+        XCTAssertEqual(error, nsError)
     }
-
-    func testExample() {
-        let testTest = TestTest()
-        
-        let error = testTest.getException().asNSError() as NSError
-//        let error = NSError(domain: "Test NSError", code: 2, userInfo: nil)
-        
-        let test = Test(nsError: error)
-        let errorExpectation = expectation(description: "Waiting for error")
-        testTest.test(test: test, crash: false) { error in
-            print(error)
-            errorExpectation.fulfill()
-        }
-        wait(for: [errorExpectation], timeout: 2)
+    
+    func testThrowNSError() {
+        let error = NSError(domain: "Test Domain", code: 1, userInfo: nil)
+        let throwable = NSErrorKt.asThrowable(error)
+        XCTAssertTrue(throwable.isNSError)
+        let nsError = NSErrorTestsKt.testThrowAsNSError(throwable: throwable) as NSError
+        XCTAssertFalse(NSErrorKt.isThrowable(nsError))
+        XCTAssertEqual(error, nsError)
     }
-
+    
+    func testThrowThrowable() {
+        let throwable = NSErrorTestsKt.getCancellationException()
+        XCTAssertFalse(throwable.isNSError)
+        let nsError = NSErrorTestsKt.testThrowAsNSError(throwable: throwable)
+        XCTAssertTrue(NSErrorKt.isThrowable(nsError))
+    }
+    
+//    func testThrowTerminatingThrowable() {
+//        let error = NSErrorTestsKt.testAsNSError()
+//        let throwable = NSErrorKt.asThrowable(error)
+//        // This will terminate the program
+//        NSErrorTestsKt.testThrowAsNSError(throwable: throwable)
+//    }
 }
--- sample/shared/src/appleMain/kotlin/com/rickclephas/kmp/nserrorkt/sample/NSErrorTests.kt
+++ sample/shared/src/appleMain/kotlin/com/rickclephas/kmp/nserrorkt/sample/NSErrorTests.kt
@@ -1,47 +1,19 @@
 package com.rickclephas.kmp.nserrorkt.sample
 
 import platform.Foundation.NSError
-import kotlin.coroutines.resumeWithException
-import kotlin.coroutines.suspendCoroutine
 import com.rickclephas.kmp.nserrorkt.*
 import kotlinx.coroutines.*
-import kotlin.native.concurrent.freeze
 
-interface NSErrorCallback {
-    fun onCompleted(error: NSError)
+fun testAsNSError(): NSError {
+    return IllegalArgumentException("Fancy exception").asNSError()
 }
 
-interface NSErrorTest {
-    fun test(callback: NSErrorCallback)
+fun getCancellationException(): CancellationException = CancellationException()
+
+fun testThrowAsNSError(throwable: Throwable): NSError {
+    return throwable.throwAsNSError(CancellationException::class)
 }
 
-object TestTest {
-
-    private val coroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Default).freeze()
-
-    fun test(test: NSErrorTest, crash: Boolean, onResult: (NSError?) -> Unit) {
-        coroutineScope.launch {
-            try {
-                testNSError(test)
-                onResult(null)
-            } catch (e: Throwable) {
-                if (crash) {
-                    onResult(e.throwAsNSError(CancellationException::class))
-                } else {
-                    onResult(e.asNSError())
-                }
-            }
-        }
-    }
-
-    fun getException() = IllegalArgumentException("Fancy exception").freeze()
-
-    private suspend fun testNSError(test: NSErrorTest) = suspendCoroutine<Unit> { cont ->
-        test.test(object : NSErrorCallback {
-            override fun onCompleted(error: NSError) {
-                cont.resumeWithException(error.asThrowable())
-            }
-        })
-    }
-
+fun testAsThrowable(nsError: NSError): Throwable {
+    return nsError.asThrowable()
 }

5090524a1ad4e077609a06c7e2fb516cec367285
Rick Clephas <rclephas@gmail.com>
2024-01-05 12:35:49 +0100

    Use default hierarchy template

--- nserror-kt/build.gradle.kts
+++ nserror-kt/build.gradle.kts
@@ -5,18 +5,16 @@ plugins {
 }
 
 kotlin {
-    val macosX64 = macosX64()
-    val macosArm64 = macosArm64()
-    val iosArm64 = iosArm64()
-    val iosX64 = iosX64()
-    val iosSimulatorArm64 = iosSimulatorArm64()
-    val watchosArm32 = watchosArm32()
-    val watchosArm64 = watchosArm64()
-    val watchosX64 = watchosX64()
-    val watchosSimulatorArm64 = watchosSimulatorArm64()
-    val tvosArm64 = tvosArm64()
-    val tvosX64 = tvosX64()
-    val tvosSimulatorArm64 = tvosSimulatorArm64()
+    listOf(
+        macosX64(), macosArm64(),
+        iosArm64(), iosX64(), iosSimulatorArm64(),
+        watchosArm32(), watchosArm64(), watchosX64(), watchosSimulatorArm64(),
+        tvosArm64(), tvosX64(), tvosSimulatorArm64(),
+    ).forEach {
+        it.compilations.getByName("main") {
+            cinterops.create("NSErrorKtRuntime")
+        }
+    }
     sourceSets {
         all {
             languageSettings {
@@ -26,32 +24,9 @@ kotlin {
                 optIn("kotlinx.cinterop.ExperimentalForeignApi")
             }
         }
-        val commonMain by getting
-        val commonTest by getting {
+        commonTest {
             dependencies {
                 implementation(kotlin("test"))
-            }
-        }
-        val appleMain by creating {
-            dependsOn(commonMain)
-        }
-        val appleTest by creating {
-            dependsOn(commonTest)
-        }
-        listOf(
-            macosX64, macosArm64,
-            iosArm64, iosX64, iosSimulatorArm64,
-            watchosArm32, watchosArm64, watchosX64, watchosSimulatorArm64,
-            tvosArm64, tvosX64, tvosSimulatorArm64
-        ).forEach {
-            it.compilations.getByName("main") {
-                cinterops.create("NSErrorKtRuntime")
-            }
-            getByName("${it.targetName}Main") {
-                dependsOn(appleMain)
-            }
-            getByName("${it.targetName}Test") {
-                dependsOn(appleTest)
             }
         }
     }
